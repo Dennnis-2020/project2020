a1707e24072752eeda6c676af4925fce9d9a344a
Dennnis-2020 <limarev.da@gmail.com>
2020-05-06 07:56:10 +0700

    Update file for Table and 2 diagram

--- MyAllWindow.py
+++ MyAllWindow.py
@@ -5,7 +5,9 @@ import shutil
 import matplotlib.pyplot as plt
 import pandas as pd
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QTextEdit, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QTextEdit, QWidget, QPushButton, QApplication, QMainWindow, QTableWidgetItem
+from PyQt5 import QtCore, QtWidgets
+import csv
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -24,21 +26,25 @@ class MyWindow(QMainWindow):
         self.textedit = QTextEdit(self)
         self.textedit.setGeometry(50,100,400,300)
 
+        self.button = QPushButton("Посмотреть таблицу", self)
+        self.button.setGeometry(250, 0, 200, 50)
+        self.button.clicked.connect(self.ShowTable)
+
         self.button = QPushButton("Выберите файл csv", self)
         self.button.setGeometry(50,50,200,50)
         self.button.clicked.connect(self.openFileDialog)
 
-        self.button = QPushButton("Cохраните файл", self)
-        self.button.setGeometry(250,50,200,50)
-        self.button.clicked.connect(self.saveFileDialog)
-
-        self.button = QPushButton("Построить диаграмму", self)
-        self.button.setGeometry(50,400,200, 50)
-        self.button.clicked.connect(self.PlotDiagram)
-
         self.button = QPushButton("Выход", self)
-        self.button.setGeometry(250, 400, 200, 50)
+        self.button.setGeometry(250,50,200,50)
         self.button.clicked.connect(sys.exit)
+
+        self.button = QPushButton("Гистограмма по кол-ву статей", self)
+        self.button.setGeometry(50,400,200, 50)
+        self.button.clicked.connect(self.PlotDiagramArt)
+
+        self.button = QPushButton("Гистограмма по цитированиям", self)
+        self.button.setGeometry(250, 400, 200, 50)
+        self.button.clicked.connect(self.PlotDiagramCit)
 
         self.setWindowIcon(QtGui.QIcon("icon.png"))
         self.setWindowTitle(self.title)
@@ -51,25 +57,55 @@ class MyWindow(QMainWindow):
             with f:
                 self.__data = f.read()
                 self.textedit.setText(self.__data)
+        __mypath = os.path.join(expanduser("~"), 'mydata.csv')
+        shutil.copyfile(filename[0], __mypath)
 
-    def saveFileDialog(self):
-        filename2 = QFileDialog.getSaveFileName(self, 'Сохраните файл', 'mydata.csv', 'Файлы csv (*.csv)')
-        if filename2[0]:
-            f = open(filename2[0], 'w', encoding='utf-8')
-            with f:
-                f.write(self.__data)
-        __mypath1 = os.path.join(expanduser("~"), 'mydat.csv')
-        shutil.copyfile(filename2[0], __mypath1)
+    def ShowTable(self):
+        self.ex = MyWidget()
+        self.ex.show()
 
-    def PlotDiagram(self):
-        __mypath = os.path.join(expanduser("~"), 'mydat.csv')
-        data = pd.read_csv(__mypath, sep=';', encoding='utf-8')
+    def PlotDiagramArt(self):
+        __mypath = os.path.join(expanduser("~"), 'mydata.csv')
+        data = pd.read_csv(__mypath, sep=';', encoding='cp1251')
         data = data.sort_values(by='Статьи РИНЦ')[:20]
         cafedra = data.values[:, 0]
         article = data.values[:, 1]
 
         plt.figure(figsize=(10, 5))
         plt.barh(cafedra, article)
-        plt.title = "Топ 20 кафедр СибГМУ, издавающих статьи в РИНЦ (апрель 2020г.)"
+        plt.title = "Топ 20 кафедр СибГМУ, издавающих статьи в РИНЦ (апрель 2020г.) по кол-ву статей"
         plt.show()
 
+    def PlotDiagramCit(self):
+        __mypath = os.path.join(expanduser("~"), 'mydata.csv')
+        data = pd.read_csv(__mypath, sep=';', encoding='cp1251')
+        data = data.sort_values(by='Цитирования РИНЦ')[:20]
+        cafedra = data.values[:, 0]
+        citation = data.values[:, 2]
+
+        plt.figure(figsize=(10, 5))
+        plt.barh(cafedra, citation)
+        plt.title = "Топ 20 кафедр СибГМУ, издавающих статьи в РИНЦ (апрель 2020г.) по цитирования"
+        plt.show()
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super(MyWidget, self).__init__()
+        __mypath = os.path.join(expanduser("~"), 'mydata.csv')
+        self.loadTable(__mypath)
+
+    def loadTable(self, __mypath):
+        self.setFixedSize(880, 600)
+        self.tableWidget = QtWidgets.QTableWidget(self)
+        self.tableWidget.setGeometry(QtCore.QRect(10, 10, 870, 600))
+        csvfile = open(__mypath, encoding="cp1251")
+        reader = csv.reader(csvfile, delimiter=';')
+        title = next(reader)
+        self.tableWidget.setColumnCount(len(title))
+        self.tableWidget.setHorizontalHeaderLabels(title)
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(reader):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount()+1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(elem))
+        self.tableWidget.resizeColumnsToContents()
